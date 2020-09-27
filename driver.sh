c26c4d4e92871f3f76d0475fc1bfcd371b07f520
rt0112358 <rt0112358@gmail.com>
2020-09-26 19:53:20 -0600

    reset feature added

--- driver.sh
+++ driver.sh
@@ -3,5 +3,6 @@
 # and the sleep timer routine.
 #########################################
 nohup google-chrome https://www.hulu.com/hub/kids&>/dev/null &
+espeak "Party time"
 konsole -e sh /home/bob/Sleep/play.sh &
 konsole -e sh /home/bob/Sleep/sleep.sh
---
+++ reset.sh
@@ -0,0 +1,17 @@
+xinput enable 2
+xinput enable 3
+xinput enable 4
+xinput enable 5
+xinput enable 6
+xinput enable 7
+xinput enable 8
+xinput enable 9
+xinput enable 10
+xinput enable 11
+xinput enable 12
+xinput enable 13
+xinput enable 14
+xinput enable 15
+pkill chrome
+espeak "Reset complete"
+pkill konsole
