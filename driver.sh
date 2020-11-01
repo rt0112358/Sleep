3e3e032649e1601a26a1b14345a0e2b7b614457a
rt0112358 <rt0112358@gmail.com>
2020-11-01 15:24:09 -0700

    Computer heckles user with a robot voice to hurry up and pick something

--- driver.sh
+++ driver.sh
@@ -3,6 +3,6 @@
 # and the sleep timer routine.
 #########################################
 nohup google-chrome https://www.hulu.com/hub/kids&>/dev/null &
-espeak "Party time"
+espeak -v en-uk-north -p 50 -s 125 "Sup dudes. I hope you are ready to pick something really fast. I am going to turn off the keyboard and mouse soon. Ready. Set. GO!"
 konsole -e sh /home/bob/Sleep/play.sh &
 konsole -e sh /home/bob/Sleep/sleep.sh
--- play.sh
+++ play.sh
@@ -6,11 +6,20 @@ echo "Hurry" | figlet | lolcat
 echo "Up" | figlet | lolcat
 echo "Press" | figlet | lolcat
 echo "Play!" | figlet | lolcat
-
-sleep 4m
-# python3 /home/bob/Sleep/press_play.py # Used to start video in fullscreen
-echo "The keyboard will be disabled in 30 seconds" | lolcat
+espeak -v en-uk-north -p 50 -s 125 "Why hello there hurry up and pick a movie"
 sleep 30s
+espeak -v en-uk-north -p 50 -s 125 "You are running out of time to pick a movie"
+sleep 30s
+espeak -v en-uk-north -p 50 -s 125 "Seriously hurry up and pick something to watch"
+sleep 2m
+espeak -v en-uk-north -p 50 -s 125 "You have less than a minute to pick a movie"
+sleep 45s
+espeak -v en-uk-north -p 50 -s 125 "I hope you picked something to watch. The buttons will not  work in a few seconds"
+sleep 10s
+
+# python3 /home/bob/Sleep/press_play.py # Used to start video in fullscreen
+echo "The keyboard will be disabled in 3 seconds" | lolcat
+sleep 3s
 xinput disable 4 #turns off touchpad
 xinput disable 5 #turns off keyboard
 xinput disable 6
